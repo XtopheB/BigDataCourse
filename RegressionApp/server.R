2f9e1b8ba85dbff366604a082335d53ea5d6d14d
Christophe Bontemps <christophe.bontemps@toulouse.inra.fr>
2023-08-09 10:56:33 +0900

    greek leters in ui, sign of coef_b1

--- RegressionApp/server.R
+++ RegressionApp/server.R
@@ -30,6 +30,9 @@ server <- function(input, output) {
     x2 = 3
     y2=  input$coef_b1 * x2 + input$coef_b0
     
+    # Sign of beta1 for display
+    sign_b1 <- ifelse(input$coef_b1 < 0, "", " +")
+    
     # generate the plot
     ggplot(data(), aes(x, y)) +
       geom_point() +
@@ -38,7 +41,7 @@ server <- function(input, output) {
       coord_cartesian(xlim = c(-3, 3), ylim = c(-3, 3)) +
       geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), colour = SIAP.color, linewidth = 2) +
       labs(x = "X", y = "Y", 
-           title = paste("Scatter Plot with Regression Line: Y =  ",input$coef_b0, "+",
+           title = paste("Scatter Plot with Regression Line: Y =  ",input$coef_b0, sign_b1,
                          input$coef_b1,"X")) +
       if (input$show_regression) {
         geom_smooth(method = "lm", se = TRUE, color = "red") 
--- RegressionApp/ui.R
+++ RegressionApp/ui.R
@@ -12,14 +12,14 @@ ui <- fluidPage(
   # Application title
   titlePanel("Playing with regression line"),
   tags$h5("Christophe Bontemps (SIAP)"),
-  tags$h5("June 2023"),
+  tags$h5("August 2023"),
   
   #titlePanel("Scatter Plot with Regression Line"),
   sidebarLayout(
     sidebarPanel(
-      HTML("<h4><font color='#2874A6'> Play with Y = b0 + b1 X </font></h4>"),
-       sliderInput("coef_b0", "Coefficient b0", min = -2, max = 2, value = -0.5, step = 0.1),
-       sliderInput("coef_b1", "Coefficient b1", min = -1, max = 2, value = dynamicValue(), step = 0.1),
+      HTML("<h4><font color='#2874A6'> Play with Y = \u03B20 + \u03B21  X </font></h4>"),
+       sliderInput("coef_b0", "Coefficient \u03B20", min = -2, max = 2, value = -0.5, step = 0.1),
+       sliderInput("coef_b1", "Coefficient \u03B21", min = -1, max = 2, value = dynamicValue(), step = 0.1),
       br(),
       HTML("<h4><font color='#2874A6'> Let's see the \"right\" result </font></h4>"),
       checkboxInput("show_regression", "Show Regression Line", value = FALSE)
